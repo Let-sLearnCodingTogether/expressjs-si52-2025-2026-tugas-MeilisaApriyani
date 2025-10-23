4db6d825b9963a6ac5ccbea5e36a1df0a61e86c2
MeilisaApriyani <melisa.2005.apriyani@gmail.com>
2025-10-23 13:09:22 +0700

    buat project

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const database = async () => {
+  try {
+    console.log("Melakukan koneksi ke MongoDB");
+
+    const response = await mongoose.connect("mongodb://127.0.0.1:27017/Meilisa?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8");
+
+    console.log(
+        `Koneksi ke MongoDB berhasil pada host: ${response.connection.host}`
+    );
+    } catch (error) {
+        console.log("Koneksi ke MongoDB gagal", error.message);
+        process.exit(1);
+    }
+};
+
+export default database;
