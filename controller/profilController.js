efbec5d0fe58b1e7428319a02675a1ac4536d9f1
MeilisaApriyani <melisa.2005.apriyani@gmail.com>
2025-10-28 10:02:06 +0700

    Update profilController.js

--- controller/profilController.js
+++ controller/profilController.js
@@ -3,7 +3,7 @@ const username = req.params.username
     res.render('public-profile', {
         title : username,
         username : username,
-        bio: "test 1234"
+        bio: "Haii, semoga saya lulus PAW2 yaa!",
     })
 }
 
